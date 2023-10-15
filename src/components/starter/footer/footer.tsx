b49dceb0f940c7dfef2db4ac96915231127814a9
YuTin <yuting1987@gmail.com>
2023-10-15 10:43:22 +0800

    feat: 🎸 init pages

--- src/components/starter/footer/footer.tsx
+++ src/components/starter/footer/footer.tsx
@@ -9,7 +9,7 @@ export default component$(() => {
     <footer>
       <div class="container">
         <a href="https://www.builder.io/" target="_blank" class={styles.anchor}>
-          <span>Made with ♡ by Builder.io</span>
+          <span>Made with ♡ by YuTin</span>
           <span class={styles.spacer}>|</span>
           <span>{serverTime.value.date}</span>
         </a>
